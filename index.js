6738e1ce96eb382bb6409f6a635417bdd6fce8e7
sky <hubcarl@126.com>
2020-09-06 13:41:43 +0800

    feat: export  KoaWebpackHotMiddleware and KoaWebpackDevMiddleware, KoaProxyMiddleware

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ module.exports = exports = require('./lib/tool');
 exports.utils = require('./lib/utils');
 exports.webpack = exports.utils.getWebpack();
 exports.merge = require('webpack-merge');
+exports.KoaWebpackHotMiddleware = require('./lib/hot');
+exports.KoaWebpackDevMiddleware = require('./lib/dev');
+exports.KoaProxyMiddleware = require('./lib/proxy');
